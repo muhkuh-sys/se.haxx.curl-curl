47a06fd15846f7ae5692ce3a2beaedf93681da4c
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-01-15 21:29:51 +0100

    Install the jonchki dependencies on the build machines. Archive the curl artifacts.

--- .docker02_build_on_ubuntu_1204.sh
+++ .docker02_build_on_ubuntu_1204.sh
@@ -11,14 +11,20 @@ mkdir -p ${PRJDIR}/build
 # Start the container and get the ID.
 ID=`docker run --detach --interactive --volume ${PRJDIR}:/tmp/work mbs_ubuntu_1204 /bin/bash`
 
+# Update the package list to prevent "not found" messages.
+docker exec ${ID} bash -c 'apt-get update --assume-yes'
+
+# Install the project specific packages.
+docker exec ${ID} bash -c 'apt-get install --assume-yes lua5.1 lua-filesystem lua-expat lua51-mhash lua-curl lua-zip'
+
 # Build the 32bit version.
 docker exec ${ID} bash -c 'cd /tmp/work && bash .build03_linux32.sh'
-docker exec ${ID} bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1204_x86.tar.gz --gzip --directory /tmp/work/build_linux32/lua5.1/lua/install .'
+docker exec ${ID} bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1204_x86.tar.gz --gzip --directory /tmp/work/build/linux32/curl/curl/install .'
 docker exec ${ID} bash -c 'chown `stat -c %u:%g /tmp/work` /tmp/work/build/build_ubuntu_1204_x86.tar.gz'
 
 # Build the 64bit version.
 docker exec ${ID} bash -c 'cd /tmp/work && bash .build04_linux64.sh'
-docker exec ${ID} bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1204_x86_64.tar.gz --gzip --directory /tmp/work/build_linux64/lua5.1/lua/install .'
+docker exec ${ID} bash -c 'tar --create --file /tmp/work/build/build_ubuntu_1204_x86_64.tar.gz --gzip --directory /tmp/work/build/linux64/curl/curl/install .'
 docker exec ${ID} bash -c 'chown `stat -c %u:%g /tmp/work` /tmp/work/build/build_ubuntu_1204_x86_64.tar.gz'
 
 # Stop and remove the container.
