63e7701ed43341e4491220d81c35b196700fbb8d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-02 16:36:18 +0100

    Install pkg-config instead of pkgconf.

--- .docker03_build_on_ubuntu_1404.sh
+++ .docker03_build_on_ubuntu_1404.sh
@@ -15,7 +15,7 @@ ID=`docker run --detach --interactive --volume ${PRJDIR}:/tmp/work mbs_ubuntu_14
 docker exec ${ID} bash -c 'apt-get update --assume-yes'
 
 # Install the project specific packages.
-docker exec ${ID} bash -c 'apt-get install --assume-yes lua5.1 lua-filesystem lua-expat lua51-mhash lua-curl lua-zip groff-base m4 pkgconf'
+docker exec ${ID} bash -c 'apt-get install --assume-yes lua5.1 lua-filesystem lua-expat lua51-mhash lua-curl lua-zip groff-base m4 pkg-config'
 
 # Build the 32bit version.
 docker exec ${ID} bash -c 'cd /tmp/work && bash .build03_linux32.sh'
