0c283da6f3d2f8ba13bc9e029917642a607d79c6
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-22 11:00:07 +0100

    Install the libraries, headers and CMake scripts as development components.

--- curl/installer/install.lua
+++ curl/installer/install.lua
@@ -3,58 +3,58 @@ local strDistId, strDistVersion, strCpuArch = t:get_platform()
 
 if strDistId=='windows' then
   if strCpuArch=='x86' then
-    t:install('windows_x86/dev/include',          '${install_dev_include}/')
-    t:install('windows_x86/dev/lib',              '${install_dev_lib}/')
-    t:install('windows_x86/dev/cmake',            '${install_dev_cmake}/')
+    t:install_dev('windows_x86/dev/include',          '${install_dev_include}/')
+    t:install_dev('windows_x86/dev/lib',              '${install_dev_lib}/')
+    t:install_dev('windows_x86/dev/cmake',            '${install_dev_cmake}/')
   elseif strCpuArch=='x86_64' then
-    t:install('windows_x86_64/dev/include',       '${install_dev_include}/')
-    t:install('windows_x86_64/dev/lib',           '${install_dev_lib}/')
-    t:install('windows_x86_64/dev/cmake',         '${install_dev_cmake}/')
+    t:install_dev('windows_x86_64/dev/include',       '${install_dev_include}/')
+    t:install_dev('windows_x86_64/dev/lib',           '${install_dev_lib}/')
+    t:install_dev('windows_x86_64/dev/cmake',         '${install_dev_cmake}/')
   end
 
 elseif strDistId=='ubuntu' then
   if strDistVersion=='12.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1204_x86/dev/include',    '${install_dev_include}/')
-      t:install('ubuntu_1204_x86/dev/lib',        '${install_dev_lib}/')
-      t:install('ubuntu_1204_x86/dev/cmake',      '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1204_x86/dev/include',    '${install_dev_include}/')
+      t:install_dev('ubuntu_1204_x86/dev/lib',        '${install_dev_lib}/')
+      t:install_dev('ubuntu_1204_x86/dev/cmake',      '${install_dev_cmake}/')
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1204_x86_64/dev/include', '${install_dev_include}/')
-      t:install('ubuntu_1204_x86_64/dev/lib',     '${install_dev_lib}/')
-      t:install('ubuntu_1204_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1204_x86_64/dev/include', '${install_dev_include}/')
+      t:install_dev('ubuntu_1204_x86_64/dev/lib',     '${install_dev_lib}/')
+      t:install_dev('ubuntu_1204_x86_64/dev/cmake',   '${install_dev_cmake}/')
     end
 
   elseif strDistVersion=='14.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1404_x86/dev/include',    '${install_dev_include}/')
-      t:install('ubuntu_1404_x86/dev/lib',        '${install_dev_lib}/')
-      t:install('ubuntu_1404_x86/dev/cmake',      '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1404_x86/dev/include',    '${install_dev_include}/')
+      t:install_dev('ubuntu_1404_x86/dev/lib',        '${install_dev_lib}/')
+      t:install_dev('ubuntu_1404_x86/dev/cmake',      '${install_dev_cmake}/')
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1404_x86_64/dev/include', '${install_dev_include}/')
-      t:install('ubuntu_1404_x86_64/dev/lib',     '${install_dev_lib}/')
-      t:install('ubuntu_1404_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1404_x86_64/dev/include', '${install_dev_include}/')
+      t:install_dev('ubuntu_1404_x86_64/dev/lib',     '${install_dev_lib}/')
+      t:install_dev('ubuntu_1404_x86_64/dev/cmake',   '${install_dev_cmake}/')
     end
 
   elseif strDistVersion=='16.04' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1604_x86/dev/include',    '${install_dev_include}/')
-      t:install('ubuntu_1604_x86/dev/lib',        '${install_dev_lib}/')
-      t:install('ubuntu_1604_x86/dev/cmake',      '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1604_x86/dev/include',    '${install_dev_include}/')
+      t:install_dev('ubuntu_1604_x86/dev/lib',        '${install_dev_lib}/')
+      t:install_dev('ubuntu_1604_x86/dev/cmake',      '${install_dev_cmake}/')
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1604_x86_64/dev/include', '${install_dev_include}/')
-      t:install('ubuntu_1604_x86_64/dev/lib',     '${install_dev_lib}/')
-      t:install('ubuntu_1604_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1604_x86_64/dev/include', '${install_dev_include}/')
+      t:install_dev('ubuntu_1604_x86_64/dev/lib',     '${install_dev_lib}/')
+      t:install_dev('ubuntu_1604_x86_64/dev/cmake',   '${install_dev_cmake}/')
     end
 
   elseif strDistVersion=='16.10' then
     if strCpuArch=='x86' then
-      t:install('ubuntu_1610_x86/dev/include',    '${install_dev_include}/')
-      t:install('ubuntu_1610_x86/dev/lib',        '${install_dev_lib}/')
-      t:install('ubuntu_1610_x86/dev/cmake',      '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1610_x86/dev/include',    '${install_dev_include}/')
+      t:install_dev('ubuntu_1610_x86/dev/lib',        '${install_dev_lib}/')
+      t:install_dev('ubuntu_1610_x86/dev/cmake',      '${install_dev_cmake}/')
     elseif strCpuArch=='x86_64' then
-      t:install('ubuntu_1610_x86_64/dev/include', '${install_dev_include}/')
-      t:install('ubuntu_1610_x86_64/dev/lib',     '${install_dev_lib}/')
-      t:install('ubuntu_1610_x86_64/dev/cmake',   '${install_dev_cmake}/')
+      t:install_dev('ubuntu_1610_x86_64/dev/include', '${install_dev_include}/')
+      t:install_dev('ubuntu_1610_x86_64/dev/lib',     '${install_dev_lib}/')
+      t:install_dev('ubuntu_1610_x86_64/dev/cmake',   '${install_dev_cmake}/')
     end
   end
 end
