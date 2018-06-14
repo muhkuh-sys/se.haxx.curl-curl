6dfb249858f4d86b89b085fa4344bc36231a347b
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-14 10:14:37 +0200

    Remove the "dev" component from the package path.

--- curl/installer/install_windows.lua
+++ curl/installer/install_windows.lua
@@ -3,9 +3,9 @@ local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
 if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' then
-  t:install_dev('dev/include',  '${install_dev_include}/')
-  t:install_dev('dev/lib',      '${install_dev_lib}/')
-  t:install_dev('dev/cmake',    '${install_dev_cmake}/')
+  t:install_dev('include',  '${install_dev_include}/')
+  t:install_dev('lib',      '${install_dev_lib}/')
+  t:install_dev('cmake',    '${install_dev_cmake}/')
   tResult = true
 end
 
