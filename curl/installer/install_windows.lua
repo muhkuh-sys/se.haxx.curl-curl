76c79bb7d73c97f743c6c867294375bc3e0d900d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-06-22 16:18:48 +0200

    Check the CPU architecture instead of the distribution version.

--- curl/installer/install_windows.lua
+++ curl/installer/install_windows.lua
@@ -2,7 +2,7 @@ local t = ...
 local strDistId, strDistVersion, strCpuArch = t:get_platform()
 local tResult
 
-if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strDistVersion=='@JONCHKI_PLATFORM_DIST_VERSION@' then
+if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strCpuArch=='@JONCHKI_PLATFORM_CPU_ARCH@' then
   t:install_dev('include',  '${install_dev_include}/')
   t:install_dev('lib',      '${install_dev_lib}/')
   t:install_dev('cmake',    '${install_dev_cmake}/')
