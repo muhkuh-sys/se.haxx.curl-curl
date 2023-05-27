141c5f818f0a50fc7b0d26018de8e19c45a21cc6
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-05-27 10:59:16 +0200

    Update buildsystem.

--- build_artifact.py
+++ build_artifact.py
@@ -1,9 +1,8 @@
-#! /usr/bin/python2.7
+#! /usr/bin/python3
 
 from jonchki import cli_args
 from jonchki import install
 from jonchki import jonchkihere
-from jonchki import vcs_id
 
 import glob
 import os
@@ -37,7 +36,7 @@ strCfg_jonchkiHerePath = os.path.join(
     'jonchki'
 )
 # This is the Jonchki version to use.
-strCfg_jonchkiVersion = '0.0.7.1'
+strCfg_jonchkiVersion = '0.0.11.1'
 # Look in this folder for Jonchki archives before downloading them.
 strCfg_jonchkiLocalArchives = os.path.join(
     strCfg_projectFolder,
@@ -210,12 +209,6 @@ strJonchki = jonchkihere.install(
     LOCAL_ARCHIVES=strCfg_jonchkiLocalArchives
 )
 
-# Try to get the VCS ID.
-strProjectVersionVcs, strProjectVersionVcsLong = vcs_id.get(
-    strCfg_projectFolder
-)
-print(strProjectVersionVcs, strProjectVersionVcsLong)
-
 
 # ---------------------------------------------------------------------------
 #
@@ -246,7 +239,18 @@ astrCmd = [
     'install-dependencies',
     '--verbose', strCfg_jonchkiVerbose,
     '--syscfg', strCfg_jonchkiSystemConfiguration,
-    '--prjcfg', strCfg_jonchkiProjectConfiguration
+    '--prjcfg', strCfg_jonchkiProjectConfiguration,
+
+    '--logfile', os.path.join(
+        strCfg_workingFolder,
+        'build_requirements',
+        'jonchki.log'
+    ),
+
+    '--dependency-log', os.path.join(
+        strCfg_projectFolder,
+        'dependency-log.xml'
+    )
 ]
 astrCmd.extend(astrJONCHKI_SYSTEM)
 astrCmd.append('--build-dependencies')
@@ -282,11 +286,11 @@ astrCMAKE_COMPILER.append('-DLIBGPG_ERROR_DIR=%s' % os.path.join(strCfg_workingF
 strNettleLib64 = os.path.join(strCfg_workingFolder, 'external', 'libnettle', 'install', 'lib64')
 strNettleLib = os.path.join(strCfg_workingFolder, 'external', 'libnettle', 'install', 'lib')
 if os.path.exists(strNettleLib64) is True:
-    shutil.copytree(strNettleLib64, strNettleLib)
+    shutil.copytree(strNettleLib64, strNettleLib, dirs_exist_ok=True)
 
 # ---------------------------------------------------------------------------
 #
-# Build the LUA5.1 version.
+# Build curl.
 #
 astrCmd = [
     'cmake',
